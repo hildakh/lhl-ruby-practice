7491f052536e1ca4bc7ec7eebc17d099990e26c2
hildakh <h_khajehamiri@yahoo.com>
2019-12-06 21:49:17 +0000

    separated the benchmark and block practice

--- blockpr.rb
+++ blockpr.rb
@@ -1,13 +1,14 @@
-require 'benchmark'
-# def benchmark
-
-# end
+def benchmark
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  running_time = end_time - start_time
+end
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
-long_string = "Hilda, Hello"*2000000
+long_string = "Hilda, Hello"*1e8
 # benchmark is a built-in module in ruby and we can require it
-running_time = Benchmark.measure { puts long_string
-}
-
+running_time = benchmark { long_string.reverse }
+# puts long_string
 puts "string.reverse took #{running_time} seconds to run"
