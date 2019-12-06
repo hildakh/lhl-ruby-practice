f70e93e0a904ce3c1a222a542b686e413ff3e7b3
hildakh <h_khajehamiri@yahoo.com>
2019-12-06 05:09:45 +0000

    arrays and branches and functions added

---
+++ arrayel34.rb
@@ -0,0 +1,7 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+bear = animals[0]
+ruby = animals[1]
+peacock = animals[2]
+kangaroo = animals[3]
+whale = animals[4]
+platypus = animals[5]
